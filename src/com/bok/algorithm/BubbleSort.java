cddd5c82c3827f6ed396f534c5631902f1c07b3a
developerBOK-tamil <developer.bok.tamil@gmail.com>
2022-05-10 22:14:45 +0530

    updated Bubble Sort
    updated Bubble Sort

--- src/com/bok/algorithm/BubbleSort.java
+++ src/com/bok/algorithm/BubbleSort.java
@@ -1,4 +1,27 @@
 package com.bok.algorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {6,5,3,1,8,7,2,4};
+        bubbleSort(array);
+        Arrays.stream(array).forEach(System.out::println);
+    }
+
+    private static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.printf(" %d Iteration \n", i+1);
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j]= array[j+1];
+                    array[j+1]= temp;
+                    System.out.printf("swapping %d %d \n", array[j], array[j+1]);
+                    Arrays.stream(array).forEach(System.out::print);
+                    System.out.println("");
+                }
+            }
+        }
+    }
 }
